414c5d5d04ddf45d530fe6c81fc3d9cfb2f2ad1d
bidduthosen <mdbidduthosen34@gmail.com>
2024-03-09 10:36:06 +0600

    local storage set card shopping

--- script/script.js
+++ script/script.js
@@ -13,5 +13,33 @@ const displayShowProduct = (productName , quantity) =>{
     const newLi  = document.createElement('li');
     newLi.innerText = `${productName}: ${quantity}`;
     productContainer.appendChild(newLi);
-
+    saveProductToLocalStorage(productName, quantity)
 }
+
+
+const getStoredShoppingCard = () =>{
+    let card = {};
+    const storedCard = localStorage.getItem('card');
+    if(storedCard){
+        card = JSON.parse(storedCard);
+    }
+    return card;
+}
+
+const saveProductToLocalStorage = (product, quantity) =>{
+    const card = getStoredShoppingCard();
+    card[product] =quantity;
+    const cardStringify = JSON.stringify(card);
+    localStorage.setItem('card', cardStringify);
+};
+
+
+const savedProductFromLocalStorage = ()=>{
+    const savedCard = getStoredShoppingCard();
+    for(const product in savedCard){
+        const quantity = savedCard[product];
+        displayShowProduct(product, quantity)
+    }
+} 
+
+savedProductFromLocalStorage()
